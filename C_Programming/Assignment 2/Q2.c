bb103f87eb4c125fd3b2dea7399e81116e449b8f
Omar Mohamed <omar.studystuff@gmail.com>
2023-07-05 22:47:19 +0300

    Create Q2.c

---
+++ C_Programming/Assignment 2/Q2.c
@@ -0,0 +1,11 @@
+//Write C program to check Vowel or Constant
+int main ()
+{
+	char letter;
+	scanf("%c",&letter);
+	if(letter == 'a' ||letter == 'A'||letter == 'e'||letter == 'E'||letter == 'i'||letter == 'I'||letter == 'o'||letter == 'O'||letter == 'u'||letter == 'U')
+		printf("Vowel");
+	else
+		printf("Constant");
+	return 0;
+}
