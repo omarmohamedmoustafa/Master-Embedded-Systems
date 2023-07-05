4aee1807abaecd1dce0112b5ea5e8cf4493495d5
Omar Mohamed <omar.studystuff@gmail.com>
2023-07-05 22:44:25 +0300

    Create Q4.c

---
+++ C_Programming/Assignment 1/Q4.c
@@ -0,0 +1,11 @@
+//EX5: Write C Program to Find ASCII Value of a Character
+#include<stdio.h>
+int main ()
+{
+	char x;
+	printf("Enter a character:");
+	fflush(stdout);
+	scanf("%c",&x);
+	printf("Character is :%d",x);
+  return 0;
+}
