a0d531ef2ef96cc10550298044e516971bc67c2f
Omar Mohamed <omar.studystuff@gmail.com>
2023-07-05 22:40:28 +0300

    Create Q9.c

---
+++ C_Programming/Midterm1/Q9.c
@@ -0,0 +1,34 @@
+/*c function to take an array and reverse its elements?*/
+#include<stdio.h>
+void reverse (int arr[],int size);
+int main()
+{
+	int size;
+	printf("Enter size of array of integers: \n");
+	fflush(stdout);
+	scanf("%d",&size);
+	int arr[size],i;
+	for(i=0;i<size;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	reverse(arr,size);
+	printf("After reversing: \n");
+	fflush(stdout);
+	for(i=0;i<size;i++)
+	{
+		printf("%d\t",arr[i]);
+	}
+}
+void reverse (int arr[],int size)
+{
+	int i,temp,tempsize=size;
+
+	for(i=0;i<size/2;i++)
+	{
+		temp=arr[i];
+		arr[i]=arr[tempsize-1];
+		arr[tempsize-1]=temp;
+		tempsize--;
+	}
+}
