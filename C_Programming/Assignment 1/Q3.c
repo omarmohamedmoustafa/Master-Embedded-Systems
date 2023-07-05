dc58561706c32f8d76afb169e7fdbaf99da43ee9
Omar Mohamed <omar.studystuff@gmail.com>
2023-07-05 22:43:41 +0300

    Create Q3.c

---
+++ C_Programming/Assignment 1/Q3.c
@@ -0,0 +1,14 @@
+/EX4:Write C Program to Multiply two Floating Point Numbers
+
+#include<stdio.h>
+int main()
+{
+	float s;
+	float d;
+	printf("Enter the two numbers:");
+	fflush(stdout);
+	scanf("%f\n",&s);
+	scanf("%f",&d);
+
+	printf("Product = %f",s*d);
+}
