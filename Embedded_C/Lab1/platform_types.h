e2f7a7a4d0aa562f4f4416b784c2d9848a2c6c51
Omar Mohamed <omar.studystuff@gmail.com>
2023-07-18 01:26:22 +0300

    Update platform_types.h

--- Embedded_C/Lab1/platform_types.h
+++ Embedded_C/Lab1/platform_types.h
@@ -13,7 +13,7 @@ typedef signed short		sint16_t;
 typedef unsigned short		uint16_t;
 
 typedef signed int			sint32_t;
-typedef unsigned int 		uint32_t;
+typedef unsigned long 		uint32_t;
 
 typedef signed long long	sint64_t;
 typedef unsigned long long	uint64_t;
